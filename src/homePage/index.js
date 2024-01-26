f77ac53cc994fda9caaf683647c5b7e954458992
vishwa <vishwa181.rejoice@gmail.com>
2024-01-26 17:23:56 +0530

    added qr code functionality

--- src/homePage/index.js
+++ src/homePage/index.js
@@ -2,21 +2,46 @@
 import React, { useState } from "react";
 import styles from "./homePage.module.scss";
 import Button from "@/shared/components/button";
-
-const backgroundImage = "assets/images/shop-back.jpg";
-
-
+import { useQRCode } from "next-qrcode";
 
 const HomePage = () => {
+  const { Canvas } = useQRCode();
+  const { Image } = useQRCode();
+
   return (
     <div className={styles.homeContainer}>
-     <div className={styles.box}>
+      <div className={styles.box}>
+        <h1>hhhh</h1>
+      </div>
 
-   <h1>hhhh</h1>
-     </div>
-
-      {/* <img src={backgroundImage} alt="background-image" /> */}
-   
+      <Canvas
+        text={"https://vi-demo-next.netlify.app/cart"}
+        options={{
+          errorCorrectionLevel: "M",
+          margin: 3,
+          scale: 4,
+          width: 200,
+          color: {
+            dark: "#fff",
+            light: "#000",
+          },
+        }}
+      />
+      <Image
+        text={` https://images.herzindagi.info/image/2022/Jan/republic-day-messages-main.jpg `}
+        options={{
+          type: "image/jpeg",
+          quality: 0.3,
+          errorCorrectionLevel: "M",
+          margin: 3,
+          scale: 4,
+          width: 200,
+          color: {
+            dark: "#fff",
+            light: "#000",
+          },
+        }}
+      />
     </div>
     // <h1>HomePage</h1>
   );
